23e0c1834991bc576b011612e00bf8167b7882df
Westley Waligora <westleywaligora@gmail.com>
2019-01-26 09:35:30 -0500

    Added an enum for the different room types.

--- GGJ2019/Assets/Scripts/RoomPickup.cs
+++ GGJ2019/Assets/Scripts/RoomPickup.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum RoomTypes
+{
+    Bathroom,
+    Bedroom,
+    Kitchen,
+    Office,
+    Floor,
+
+    RoomTypesCount
+}
+
 /// <summary>
 /// Extension of the PickupComponent which represents any pickup-able component that is part of a room.
 /// </summary>
-public class RoomPickup : PickupComponent {
-
+public class RoomPickup : PickupComponent
+{
+    [SerializeField]
+    RoomTypes RoomType;
 }
