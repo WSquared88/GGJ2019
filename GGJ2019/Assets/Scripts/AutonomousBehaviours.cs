eeea55d8c412e91803b7054235575d7fce85dffc
Dan Singer <djs5435@rit.edu>
2019-01-26 13:10:38 -0500

    Develop AutonomousBehaviours script

---
+++ GGJ2019/Assets/Scripts/AutonomousBehaviours.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Collection of autonomous agent behaviours. These return vectors, so it's up to the calling code to use them.
+/// </summary>
+/// 
+[RequireComponent(typeof(Rigidbody))]
+public class AutonomousBehaviours : MonoBehaviour
+{
+
+    public float MaxSpeed = 10;
+    private Rigidbody Rb;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    public Vector3 Seek(Vector3 target)
+    {
+        Vector3 desired_velocity = (target - transform.position).normalized * MaxSpeed;
+        Vector3 steer = desired_velocity - Rb.velocity;
+        return steer;
+    }
+    public Vector3 Flee(Vector3 target)
+    {
+        Vector3 desired_velocity = (transform.position - target).normalized * MaxSpeed;
+        Vector3 steer = desired_velocity - Rb.velocity;
+        return steer;
+    }
+    public Vector3 Arrive(Vector3 target, float slowingDistance)
+    {
+        Vector3 target_offset = target - transform.position;
+        float distance = target_offset.magnitude;
+        float ramped_speed = MaxSpeed * (distance / slowingDistance);
+        float clipped_speed = Mathf.Min(ramped_speed, MaxSpeed);
+        Vector3 desired_velocity = (clipped_speed / distance) * target_offset;
+        Vector3 steer = desired_velocity - Rb.velocity;
+        return steer;
+    }
+
+    public Vector3 Wander(float distanceAhead, float radius)
+    {
+        Quaternion rotator = Quaternion.Euler(0, Random.Range(-180, Mathf.PerlinNoise(Time.time, 0) * 180), 0);
+        Vector3 randOffset = rotator * transform.forward * radius;
+        print(randOffset);
+        Vector3 target = transform.forward * distanceAhead + randOffset;
+        return Seek(transform.position + target);
+    }
+
+    public Vector3 Constrain(Bounds bounds)
+    {
+        Vector3 min = bounds.center - bounds.extents;
+        Vector3 max = bounds.center + bounds.extents;
+        Vector3 desired_velocity = Vector3.zero;
+        if (transform.position.x < min.x || transform.position.x > max.x)
+            desired_velocity.x = -Rb.velocity.x;
+        if (transform.position.y < min.y || transform.position.y > max.y)
+            desired_velocity.y = -Rb.velocity.y;
+        if (transform.position.z < min.z || transform.position.z > max.z)
+            desired_velocity.z = -Rb.velocity.z;
+        Vector3 steer = desired_velocity - Rb.velocity;
+        return steer;
+    }
+
+}
--- GGJ2019/Assets/Scripts/Person.cs
+++ GGJ2019/Assets/Scripts/Person.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 /// <summary>
 /// Person component which maintains likes, dislikes, and timer status for People.
 /// </summary>
+[RequireComponent(typeof(AutonomousBehaviours))]
 public class Person : MonoBehaviour
 {
     public RoomPickup[] Likes;
@@ -21,6 +22,7 @@ public class Person : MonoBehaviour
     [Tooltip("Used in sell value calculation. This is multiplied by BuyerTime and default Sell Value to get adjusted sell value")]
     public float PercentModifier = 1;
 
+    public Collider WanderArea;
 
     // TODO the house should inform the Person about its state, and then these values should be calculated based on that.
     private float CurrentLikes = 0;
@@ -31,10 +33,13 @@ public class Person : MonoBehaviour
     /// </summary>
     public float BuyerTime { get; private set; }
 
+    private AutonomousBehaviours AutoBehav;
+    private Rigidbody Rb;
     void Start ()
     {
         BuyerTime = MaxTimerValue;
-
+        Rb = GetComponent<Rigidbody>();
+        AutoBehav = GetComponent<AutonomousBehaviours>();
         // For testing
         TimerDepleted += () => Debug.Log("Timer depleted");
 	}
@@ -43,10 +48,18 @@ public class Person : MonoBehaviour
     {
         BuyerTime -= Time.deltaTime * CurrentDislikes;
     }
+
+    void Move()
+    {
+        Rb.AddForce(AutoBehav.Wander(20, 5));
+        Rb.AddForce(AutoBehav.Constrain(WanderArea.bounds) * 5);
+        transform.forward = Rb.velocity.normalized;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
         if (BuyerTime > 0)
         {
             ProgressTimer();
