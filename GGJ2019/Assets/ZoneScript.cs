e4e2d4ea19c482b4a0db20a03c665f79aef02864
Robert <emailrobertb@gmail.com>
2019-01-25 21:23:43 -0500

    Sell Zone framework
    Sell zone framework on collision enter. Requires house to have tag "house"

---
+++ GGJ2019/Assets/ZoneScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider entrant)
+	{
+        //foreach(ContactPoint contact in collision.contacts)
+        //{};
+        if (entrant.gameObject.tag == "house")
+        {
+            Debug.Log("Collision Entered");
+        }
+	}
+	
+}
+
