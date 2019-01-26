2490b5d24e65b24a8d873e46942dfc78d8e37512
Dan Singer <djs5435@rit.edu>
2019-01-26 18:01:53 -0500

    Create very basic game over screen

---
+++ GGJ2019/Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Collection of UI Functions not tied to any particular object
+/// </summary>
+public class UIFunctions : MonoBehaviour {
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
