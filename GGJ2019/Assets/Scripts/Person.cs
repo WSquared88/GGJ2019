a8e0adbf3a696c6d91deff82faec9dd2e7fa09f4
Dan Singer <djs5435@rit.edu>
2019-01-25 20:48:12 -0500

    Begin Person class

---
+++ GGJ2019/Assets/Scripts/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Person component which maintains likes, dislikes, and timer status for People.
+/// </summary>
+public class Person : MonoBehaviour
+{
+
+
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
