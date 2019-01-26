1f49054200cb8148ac541f7f1b5115f0e4032110
Dan Singer <djs5435@rit.edu>
2019-01-25 21:11:18 -0500

    Clean up Person class

--- GGJ2019/Assets/Scripts/Person.cs
+++ GGJ2019/Assets/Scripts/Person.cs
@@ -17,17 +17,23 @@ public class Person : MonoBehaviour
 
     public List<RoomPickup> Likes { get; private set; }
     public List<RoomPickup> Dislikes { get; private set; }
+
     public event Action TimerDepleted;
+    private bool TimerDepletedEventFired = false;
+
     [Tooltip("The maximum value of the timer that this person will use.")]
     public float MaxTimerValue = 10;
+
     [Tooltip("Value that will be multiplied by DeltaTime and subtracted from the timer each frame.")]
     public float TimerDecrementMultiplier = 1; 
+
     private float Timer;
-    private bool TimerDepletedEventFired = false;
 
     void Start ()
     {
         Timer = MaxTimerValue;
+
+        // For testing
         TimerDepleted += () => Debug.Log("Timer depleted");
 	}
 
