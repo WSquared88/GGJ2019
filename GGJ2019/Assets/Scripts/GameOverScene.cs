e6907485407d95de108efbacb6d346ad642bfcd9
Dan Singer <djs5435@rit.edu>
2019-01-27 12:57:07 -0500

    Implement game over audio

--- GGJ2019/Assets/Scripts/GameOverScene.cs
+++ GGJ2019/Assets/Scripts/GameOverScene.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOverScene : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class GameOverScene : MonoBehaviour
+{
+    public AudioManager Audio;
+    public float ScoreToWin = 10000;
+    void Start()
+    {
+        if (ScoreManager.currScore < ScoreToWin)
+        {
+            Audio.SourceA.clip = Audio.LoseTheme;
+        }
+        else
+        {
+            Audio.SourceA.clip = Audio.WinTheme;
+        }
+        Audio.SourceA.Play();
+    }
 }
